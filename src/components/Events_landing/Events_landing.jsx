8c12a8421bd5d5437822fbbe0a75db885c071da4
AniD-z <137194673+AniD-z@users.noreply.github.com>
2023-11-08 11:58:30 +0530

    Update Events_landing.jsx

--- src/components/Events_landing/Events_landing.jsx
+++ src/components/Events_landing/Events_landing.jsx
@@ -16,7 +16,7 @@ export default function Events_landing() {
                 <p>
                 There's been a Murder in SQL City! The Murder Mystery is designed to be both a self-directed lesson to learn NoSQL concepts and commands and a fun game for experienced NoSQL users to solve an intriguing crime.
                 </p>
-                <a href="input.html" class="read-more">
+                <a href="./input.html" class="read-more">
                   Start-now{" "}
                   <span class="sr-only">about this is some title</span>
                   <svg
