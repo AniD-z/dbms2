4b855cf624890640c8a347e32aa934c18004adcc
Hitesh918 <118730681+Hitesh918@users.noreply.github.com>
2023-12-23 14:28:03 +0530

    Update input.jsx

--- src/components/event_forms/input.jsx
+++ src/components/event_forms/input.jsx
@@ -1,125 +1,64 @@
+import React from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import {autocompletion} from "@codemirror/autocomplete"
+import { monokai } from '@uiw/codemirror-theme-monokai';
+
 import { useState } from "react";
 import Axios from "axios";
 import "./form.css";
 
 function Input() {
-  // const [teamName, setTeamName] = useState("");
-  // const [teamCaptain, setTeamCaptain] = useState("");
-  // const [regNo1, setRegNo1] = useState("");
-  // const [teamMate2, setTeamMate2] = useState("");
-  // const [regNo2, setRegNo2] = useState("");
-  // const [teamMate3, setTeamMate3] = useState("");
-  // const [regNo3, setRegNo3] = useState("");
-  // const [teamMate4, setTeamMate4] = useState("");
-  // const [regNo4, setRegNo4] = useState("");
-  // const [sub, setSub] = useState("");
-  // const [regNo5, setRegNo5] = useState("");
-  // const [captainIgName, setCaptainIgName] = useState("");
-  // const [TxId, setTxId] = useState("");
+   const [value, setValue] = React.useState("console.log('hello world!');");
+  const onChange = React.useCallback((val, viewUpdate) => {
+    console.log('val:', val);
+    setValue(val);
+  }, []);
 
-  // async function registerBgmiUser(event) {
-  //   event.preventDefault();
-  //   const res = await Axios.post(
-  //     "https://forge-backend-service.onrender.com/bgmi",
-  //     {
-  //       teamName: teamName,
-  //       teamCaptain: teamCaptain,
-  //       regNo1: regNo1,
-  //       teamMate2: teamMate2,
-  //       regNo2: regNo2,
-  //       teamMate3: teamMate3,
-  //       regNo3: regNo3,
-  //       teamMate4: teamMate4,
-  //       regNo4: regNo4,
-  //       sub: sub,
-  //       regNo5: regNo5,
-  //       captainIgName: captainIgName,
-  //       TxId: TxId,
-  //     },
-  //     {
-  //       header: {
-  //         "content-type": "application/json",
-  //       },
-  //     }
-  //   ).then(() => (window.location.href = "/succuss"));
-  // }
-    const clearLog = () => {
-    // Your clearLog implementation
-      console.log("qwert")
-  };
+  const completions = [
+  {label: "panic", type: "keyword"},
+  {label: "park", type: "constant", info: "Test completion"},
+  {label: "password", type: "variable"},
+  {label: "get_fit_now_member", type: "keyword"}
+]
+  function myCompletions(context) {
+  let before = context.matchBefore(/\w+/)
+  // If completion wasn't explicitly started and there
+  // is no word before the cursor, don't open completions.
+  if (!context.explicit && !before) return null
+  return {
+    from: before ? before.from : context.pos,
+    options: completions,
+    validFor: /^\w*$/
+  }
+}
 
-  const launch = () => {
-    // Your launch implementation
-          console.log("qwertyuiop")
+  //   const clearLog = () => {
+  //   // Your clearLog implementation
+  //     console.log("qwert")
+  // };
 
-  };
+  // const launch = () => {
+  //   // Your launch implementation
+  //         console.log("qwertyuiop")
+
+  // };
 
   return (
  <div>
+    <CodeMirror
+      value={value}
+      height="20rem"
+      width='60rem'
+      
+      theme={monokai}
+      extensions={[javascript({ jsx: true }) , autocompletion({override: [myCompletions]}) 
 
-      <h1>TESTING AREA</h1>
-
-      <table>
-        <tbody>
-
-          <tr>
-            <td>
-              <p>INPUT</p>
-            </td>
-            <td>
-              <input id="input1" />
-              <input id="input2" />
-              <input id="input3" />
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <p>OUTPUT</p>
-            </td>
-            <td>
-              <p id="output"></p>
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <p>LOG</p>
-            </td>
-            <td>
-              <p id="log"></p>
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <button onClick={clearLog}>Clear the log</button>
-            </td>
-            <td>
-              <button onClick={launch}>Execute launch()</button>
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <p>QUICK NOTES</p>
-            </td>
-            <td>
-              <p className="noteType">IDs</p>
-              <p>Input text box: "input"</p>
-              <p>Output text: "output"</p>
-              <p>Log text: "log"</p>
-              <p className="noteType">FUNCTIONS</p>
-              <p>On button press: "launch()"</p>
-              <p>Access the debug: debug(x)</p>
-              <p>Clear the log: clearLog()</p>
-            </td>
-          </tr>
-
-        </tbody>
-      </table>
-
-    </div>
+    ]} 
+      onChange={onChange}
+    />
+ 
+   </div>
   );
 }
 
