cae758d9b574b2172f6843bbaa91c78c22ff7b38
Hitesh918 <118730681+Hitesh918@users.noreply.github.com>
2023-12-24 11:23:37 +0530

    Update input.jsx

--- src/components/event_forms/input.jsx
+++ src/components/event_forms/input.jsx
@@ -6,6 +6,8 @@ import { monokai } from '@uiw/codemirror-theme-monokai';
 
 import { useState } from "react";
 import Axios from "axios";
+import Button from '@mui/material/Button';
+
 import "./form.css";
 
 function Input() {
@@ -14,7 +16,16 @@ function Input() {
     console.log('val:', val);
     setValue(val);
   }, []);
-
+   
+  async function handleSubmit(){
+    const response = await Axios.get("https://schemaverse.onrender.com/" , {
+      params : {
+        input : value
+      }
+    })
+    console.log(response)
+  }
+   
   const completions = [
   {label: "panic", type: "keyword"},
   {label: "park", type: "constant", info: "Test completion"},
@@ -57,6 +68,8 @@ function Input() {
     ]} 
       onChange={onChange}
     />
+    <Button onClick={handleSubmit} variant="contained">SUBMIT</Button>
+
  
    </div>
   );
