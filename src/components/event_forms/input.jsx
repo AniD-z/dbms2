dedf98e2364be43c7c665d4a7dcdeeedeaf6b280
AniD-z <137194673+AniD-z@users.noreply.github.com>
2024-01-05 16:03:57 +0530

    Update input.jsx

--- src/components/event_forms/input.jsx
+++ src/components/event_forms/input.jsx
@@ -76,8 +76,7 @@ function Input() {
                 ]}
                 onChange={onChange}
             />
-            <Button onClick={handleSubmit} variant="contained">SUBMIT</Button>
-            <button type="submit" className="valorant-btn">
+            <button onClick={handleSubmit} variant="contained" type="submit" className="valorant-btn">
           <span class="underlay">
             <span class="label">Submit</span>
           </span>
