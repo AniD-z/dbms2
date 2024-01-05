9861f093fdac888666eae9e39a20c2cba451cdee
AniD-z <137194673+AniD-z@users.noreply.github.com>
2024-01-05 21:51:29 +0530

    Update input.jsx

--- src/components/event_forms/input.jsx
+++ src/components/event_forms/input.jsx
@@ -9,23 +9,21 @@ import { monokai } from '@uiw/codemirror-theme-monokai';
 import { useState, useEffect } from "react";
 import Axios from "axios";
 import { useLocation } from 'react-router-dom';
+import { blue } from "@mui/material/colors";
 
 
 function Input() {
     let  state = useLocation();
 
     useEffect(() => {
-        document.body.style.backgroundColor = "#000000";
+        document.body.style.backgroundColor = "#000000"
         document.body.style.paddingLeft = "10rem";
-        document.body.style.paddingRight="10rem";
-        console.log("bitchhhh");
-        console.log(state);
-        
+        document.body.style.paddingLeft="10rem"
+        console.log("bitchhhh")
+        console.log(state)
+        // console.log(passedData)
 
     }, [])
-    const editorStyle = {
-        fontSize: "1.5rem",
-      };
 
 
     const [value, setValue] = React.useState("db.collection.operation(query) \n \n \n \n \n");
@@ -42,7 +40,9 @@ function Input() {
         })
         console.log(response)
     }
-
+    const editorStyle = {
+        fontSize: "1.5rem"
+    }
     const completions = [
         { label: "panic", type: "keyword" },
         { label: "park", type: "constant", info: "Test completion" },
@@ -51,6 +51,8 @@ function Input() {
     ]
     function myCompletions(context) {
         let before = context.matchBefore(/\w+/)
+        // If completion wasn't explicitly started and there
+        // is no word before the cursor, don't open completions.
         if (!context.explicit && !before) return null
         return {
             from: before ? before.from : context.pos,
@@ -65,6 +67,10 @@ function Input() {
             <p>{passedData.prob}</p> */}
             <CodeMirror
                 value={value}
+                backgroundColor ="blue"
+
+    
+                
                 width='60rem'
                 style={editorStyle}
                 theme={monokai}
