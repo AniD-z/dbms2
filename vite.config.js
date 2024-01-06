f6a32fcc6687827f62b774ee2e2b307220d7a81a
AniD-z <137194673+AniD-z@users.noreply.github.com>
2024-01-06 10:07:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-    },
-  },
+
   plugins: [react()],
 });
