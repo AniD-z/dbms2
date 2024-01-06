f2b3bf6290d7682f24c130f337f58710b13e9b63
AniD-z <137194673+AniD-z@users.noreply.github.com>
2024-01-06 10:28:40 +0530

    j

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path"; // Add this line to import the path module
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-  plugins: [react()]
-})
+  plugins: [react()],
+});
